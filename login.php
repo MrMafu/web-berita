8dc03ca92661441a90a3b1692a570bcc3fdc1e8a
MrMafu <kinanradiaputra@gmail.com>
2024-06-01 19:33:20 +0700

    Reworked

--- login.php
+++ login.php
@@ -6,11 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/style.css">
+    <script src="https://kit.fontawesome.com/90c067ab0f.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <a href="index.php"><button>Home</button></a>
     <h1>Login</h1>
+    <a href="index.php"><button><i class="fa-solid fa-chevron-left"></i></button></a>
     <form class="field" action="control.php" method="post">
         <label for="username">Username</label>
         <input type="text" name="username" required>
--- welcome.php
+++ welcome.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/style.css">
+    <script src="https://kit.fontawesome.com/90c067ab0f.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <h1>Looks like you don't have an account</h1>
